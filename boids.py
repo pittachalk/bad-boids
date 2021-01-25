fdabb5ff77a5aa8d8b4752c1ddc9f6901336ab81
pittachalk <>
2021-01-25 16:48:01 +0000

    run black to fix spaces and formatting

--- boids.py
+++ boids.py
@@ -14,46 +14,51 @@ boids_y = uniform(300, 600, n_birds)
 boid_x_velocities = uniform(0, 10, n_birds)
 boid_y_velocities = uniform(-20, 20, n_birds)
 
-boids = (boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+
 
 def update_boids(boids):
-	xs,ys,xvs,yvs=boids
-	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
-	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
-	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
+    xs, ys, xvs, yvs = boids
+
+    # Fly towards the middle
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            xvs[i] = xvs[i] + (xs[j] - xs[i]) * 0.01 / len(xs)
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            yvs[i] = yvs[i] + (ys[j] - ys[i]) * 0.01 / len(xs)
+
+    # Fly away from nearby boids
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
+                xvs[i] = xvs[i] + (xs[i] - xs[j])
+                yvs[i] = yvs[i] + (ys[i] - ys[j])
+
+    # Try to match speed with nearby boids
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
+                xvs[i] = xvs[i] + (xvs[j] - xvs[i]) * 0.125 / len(xs)
+                yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * 0.125 / len(xs)
+
+    # Move according to velocities
+    for i in range(len(xs)):
+        xs[i] = xs[i] + xvs[i]
+        ys[i] = ys[i] + yvs[i]
 
 
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
+figure = plt.figure()
+axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
+scatter = axes.scatter(boids[0], boids[1])
+
 
 def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(list(zip(boids[0],boids[1])))
+    update_boids(boids)
+    scatter.set_offsets(list(zip(boids[0], boids[1])))
 
 
-anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
+anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
 
 if __name__ == "__main__":
     plt.show()
--- test_boids.py
+++ test_boids.py
@@ -3,11 +3,13 @@ from nose.tools import assert_almost_equal
 import os
 import yaml
 
+
 def test_bad_boids_regression():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
+    regression_data = yaml.load(
+        open(os.path.join(os.path.dirname(__file__), "fixture.yml"))
+    )
+    boid_data = regression_data["before"]
     update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
-	
+    for after, before in zip(regression_data["after"], boid_data):
+        for after_value, before_value in zip(after, before):
+            assert_almost_equal(after_value, before_value, delta=0.01)
