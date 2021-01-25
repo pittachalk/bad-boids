00edbe43300f2b5dcedef59a1c5edbd9e183d9df
pittachalk <>
2021-01-25 16:26:54 +0000

    add variable for n_birds

--- boids.py
+++ boids.py
@@ -7,12 +7,12 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
-# Deliberately terrible code for teaching purposes
+n_birds = 50
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+boids_x=[random.uniform(-450,50.0) for x in range(n_birds)]
+boids_y=[random.uniform(300.0,600.0) for x in range(n_birds)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(n_birds)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(n_birds)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
